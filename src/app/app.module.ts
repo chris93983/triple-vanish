fc7468128593dfe242af37c3f50dfd5a499e1769
Song <sky93983@sina.com>
2022-10-01 16:30:57 +0800

    Add core/shared module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -10,7 +12,9 @@ import { AppRoutingModule } from './app-routing.module';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        CoreModule,
+        SharedModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { DeviceService } from './services/device.service';
 
 @NgModule({
     declarations: [],
     imports: [CommonModule],
-    providers: [DeviceService],
+    providers: [],
 })
-export class CoreModule { }
+export class SharedModule { }
