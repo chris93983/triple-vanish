55e4fb61dfb8821a3912093bc2385f420ee7a74b
Song <sky93983@sina.com>
2022-10-02 16:06:47 +0800

    Add food animation.

--- src/app/account/components/login/login.component.ts
+++ src/app/account/components/login/login.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'tv-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export class LoginComponent { }
+export class LoginComponent implements AfterViewInit {
+    @ViewChild('food', { static: true }) food: ElementRef<HTMLElement> | null = null;
+
+    ngAfterViewInit(): void {
+        this.food?.nativeElement.addEventListener('animationend', () => this.food?.nativeElement.classList.add('shadow'));
+        this.food?.nativeElement.addEventListener('animationstart', () => this.food?.nativeElement.classList.add('show-food'));
+        this.food?.nativeElement.classList.add('animate');
+    }
+}
